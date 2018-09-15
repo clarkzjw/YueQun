dcaca9407deffe067f5727b652ac761080f99dde
clarkzjw <i@jinwei.me>
2018-09-15 23:41:25 +0800

    + fix mysql dir

--- start-mysql.sh
+++ start-mysql.sh
@@ -1,3 +1,2 @@
-
+docker run --name yqbot-mysql -p 3306:3306 -v ./data/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=mysql-pw-secret -d mysql:8.0.12
 CREATE DATABASE yuequnbot CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-docker run --name yqbot-mysql -p 3306:3306 -v /app/data/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=mysql-pw-secret -d mysql:8.0.12

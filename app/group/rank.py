4ddf5072507f52954a2ea09c94f7d7d42cee7028
clarkzjw <i@jinwei.me>
2018-10-01 21:54:05 +0800

    + fix rank username

--- app/group/rank.py
+++ app/group/rank.py
@@ -1,11 +1,16 @@
 from pony.orm.core import select, count, desc
 
-from model.db import db_session, Message
+from model.db import db_session, Message, User
 
 
 def get_rank(args):
     with db_session:
-        all_messages = list(select((msg.tg_user_username, count()) for msg in Message).order_by(lambda: desc(count())))
+        all_users = list(select((user.tg_user_id, user.tg_user_username) for user in User))
+        users = {}
+        for u in all_users:
+            users[u[0]] = u[1]
+
+        all_messages = list(select((msg.tg_user_id, count()) for msg in Message).order_by(lambda: desc(count())))
         number = len(all_messages)
         try:
             number = abs(int(args[0]))
@@ -14,6 +19,6 @@ def get_rank(args):
         rank_text = ""
         c = 1
         for r in all_messages[:number]:
-            rank_text += "*{}. {}* => {}\n".format(c, r[0], r[1])
+            rank_text += "*{}. {}* => {}\n".format(c, users[r[0]], r[1])
             c += 1
         return rank_text

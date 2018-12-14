8215497fb473334c67d53c9ef7a14ac509691b0b
clarkzjw <i@jinwei.me>
2018-12-14 14:30:10 +0800

    + add shell

--- start-mysql.sh
+++ start-mysql.sh
@@ -1,2 +1,4 @@
-docker run --name yqbot-mysql -p 3306:3306 -v ./data/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=mysql-pw-secret -d mysql:8.0.12
-CREATE DATABASE yuequnbot CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+#!/usr/bin/env bash
+
+sudo docker run  --name mysql -p 33070:3306  -v <path>:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=pass -d mysql:5.7.17
+# CREATE DATABASE yuequnbot CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
